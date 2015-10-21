1c8eadcf8caf19723225ce0b0414af79d524fc35
Tyler <tyler@sliceofnet.com>
2015-10-21 17:07:53 -0400

    Can create users now.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,14 @@
 class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
-  has_secure_password
   #Associations
   has_many :posts
 
   #Accessors
-  attr_accessor :name, :email
 
   #Validations
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :name,  presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
