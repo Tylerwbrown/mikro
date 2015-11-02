81e05c98d832c63ccc23fb18500aeb681b109476
Tylerwbrown <tyler@sliceofnet.com>
2015-11-02 17:57:57 -0500

    Comments!

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
   belongs_to :user
   default_scope -> { order(created_at: :desc) }
+  has_many :comments
 
   validates :user_id, presence: true
   validates :content, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151028172448) do
+ActiveRecord::Schema.define(version: 20151102212848) do
+
+  create_table "comments", force: true do |t|
+    t.string   "user_name"
+    t.text     "body"
+    t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "posts", force: true do |t|
     t.text     "content"
